34d5cfd120f5b9fb3ce768e825d91c14acc058b3
Alex Faican <lizer12f@gmail.com>
2023-01-08 17:20:33 -0500

    Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp1Component } from './Componentes/Componente1/comp1/comp1.component';
+import { Comp2Component } from './Componentes/Componente2/comp2/comp2.component';
+import { Comp3Component } from './Componentes/Componente3/comp3/comp3.component';
+import { Comp4Component } from './Componentes/Componente4/comp4/comp4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    Comp2Component,
+    Comp3Component,
+    Comp4Component
   ],
   imports: [
     BrowserModule,
